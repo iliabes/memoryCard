a1dd97eeccf760a412d2f14c58f3e3e4bdbd46e2
iliabes <oksana14888@gmail.com>
2022-06-02 14:47:54 +0500

    +light img

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,5 +1,6 @@
 import './Card.sass'
 import backFace from '../img/img/4.png'
+import bg1 from '../img/spacemarine/spaceMarine4.jpg'
 
 
 
@@ -9,7 +10,7 @@ export default function Card(props) {
     return (
       <div data-pers={props.link} className="content-card">
       <div className="back">
-        <img  alt={props.link} src={require('../img/'+props.link+'.jpeg')} />
+        <img  alt={props.link} src={require('../img/'+props.link+'.jpg')} />
       </div>
       <div className="front">
         <img onClick={props.funcAddClass} alt={'back face card'} src={backFace}/>
@@ -17,3 +18,5 @@ export default function Card(props) {
       </div>
     );
 }
+
+// require('../img/'+props.link+'.jpg')
